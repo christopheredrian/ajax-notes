67f15b15418f8deda31a0385071a354359cf544d
Chirstopher Espiritu <christopheredrian@gmail.com>
2015-10-15 02:02:26 -0400

    Add notes on form manipulation and JSON support with AJAX

--- js/ajax-forms.js
+++ js/ajax-forms.js
@@ -11,15 +11,46 @@ $(document).ready(function(){
 			// PULLS ALL THE DATA OUT OF THE FORM
 			data: registerForm.serialize(),
 			success: function(response){
-				alert('registered!');
-				registerForm.remove();
-				$("#responseDiv").hide().html(response).fadeIn();
+				try {
+					var respJson =  JSON.parse(response);
+					console.log(respJson);
+					$("#responseDivJson").hide().html(JSON.stringify(respJson)).fadeIn();
+				} catch(error) {
+					console.log('on catch');
+					showAsHtml(response);
+				}
+
 			},
 			error: function(response, errorType, errorMessage){
-				alert("There was an error!" + errorMessage);
+				console.log("There was an error!" + errorMessage);
 			}
 		});
 
+		function showAsHtml(response){
+			console.log('registered!');
+			registerForm.remove();
+			$("#responseDiv").hide().html(response).fadeIn();
+		}
+
+	});
+
+
+	// AJAX with PURE JSON
+	$('#jsonRequestBtn').on('click', function(){
+		$.ajax('person.json', {
+			type: 'GET',
+			dataType: 'json',
+			success: function(response){
+				$('#personResponseJson').html(JSON.stringify(response));
+				// Barbaric! haha atleast do concatenation instead!
+				$('#personResponseJson').append('<br>Id: ' + response.id);
+				$('#personResponseJson').append('<br>First Name: ' + response.firstName);
+				$('#personResponseJson').append('<br>Last Name: ' + response.lastName);
+				$('#personResponseJson').append('<br>age: ' + response.age);
+
+			},
+			contentType: 'application/json' // Tells the server that we want a JSON format
+		});
 	});
 
 });
--- register.php
+++ register.php
@@ -1,7 +1,18 @@
-<p>Thank you for registering</p>
-<p>These are your details: </p>
-<?php 
+<?php if (isset($_POST['response-json'])): ?>
+	<?php if ($_POST['response-json'] === 'on'): ?>
+		<?php
+		$arr = [];
+		foreach ($_POST as $key => $value) {
+			$arr[$key] = $value;
+		}
+		echo json_encode($arr);
+		?>
+	<?php endif ?>
+<?php else: ?>
+	<p>Thank you for registering</p>
+	<p>These are your details: </p>
+	<?php foreach ($_POST as $key => $value): ?>
+		<?php echo htmlspecialchars($key) . " is " . htmlspecialchars($value) . "<br>" ?>
+	<?php endforeach ?>
+<?php endif ?>
 
-foreach ($_POST as $key => $value)
-	echo htmlspecialchars($key) . " is " . htmlspecialchars($value) . "<br>";
-?>
